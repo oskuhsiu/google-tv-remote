b5a9abc44c78eb14fe134abbfe5562d0afe55c4a
oskuhsiu <osku.hsiu@gmail.com>
2026-09-02 16:43:05 +0800

    Release 0.1.4

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "io.github.oskuhsiu.googletvremote"
         minSdk = 26
         targetSdk = 36
-        versionCode = 7
-        versionName = "0.1.4-beta.3"
+        versionCode = 8
+        versionName = "0.1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
