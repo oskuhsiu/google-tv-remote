e5d806555baec4a2c17a0e4e59211faf0c556d0b
oskuhsiu <osku.hsiu@gmail.com>
2026-09-02 16:26:03 +0800

    Update app icon for beta.2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "io.github.oskuhsiu.googletvremote"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.1.4-beta.1"
+        versionCode = 6
+        versionName = "0.1.4-beta.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
